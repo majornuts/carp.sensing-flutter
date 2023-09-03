d5760ec0dadc898cebbdde9f47a4fa06f6805731
majornuts <mads.fisker@gmail.com>
2023-09-03 11:45:45 +0200

    work in progress

--- carp_mobile_sensing/example/ios/Flutter/flutter_export_environment.sh
+++ carp_mobile_sensing/example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/majornuts/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/majornuts/StudioProjects/carp.sensing-flutter/carp_mobile_sensing/example"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\major\StudioProjects\carp.sensing-flutter\carp_mobile_sensing\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.40.9"
 export "FLUTTER_BUILD_NUMBER=0.40.9"
